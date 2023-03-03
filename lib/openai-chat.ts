4a46a37e731253335003cfa1401db36ea4c689e6
Scionax <ScionaxNG@gmail.com>
2023-03-03 17:08:17 -0600

    Create openai-chat.ts

---
+++ lib/openai-chat.ts
@@ -0,0 +1,50 @@
+
+type ChatMessages = Array<ChatObject>;
+
+type ChatRole = "user" | "system" | "assistant";
+
+type ChatObject = {
+	role: ChatRole,
+	content: string,
+};
+
+export class OpenAIChat {
+	public messages: ChatMessages = [];
+	
+	// Alias for setSystemChat()
+	addSystemChat(content: string): void {
+		return this.setSystemChat(content);
+	}
+	
+	setSystemChat(content: string): void {
+		if(!this.messages.length) {
+			this.addChat("system", content);
+			return;
+		}
+		
+		this.messages[0].content = content;
+	}
+	
+	addUserChat(content: string): ChatObject {
+		return this.addChat("user", content);
+	}
+	
+	addAssistantChat(content: string): ChatObject {
+		return this.addChat("assistant", content);
+	}
+	
+	private addChat(role: ChatRole, content: string): ChatObject {
+		if(role !== "system" && !this.messages.length) {
+			this.setSystemChat("React to the conversation based on your best interpretation of what is desired by the user.");
+		}
+		
+		const chatObject: ChatObject = {
+			role: role,
+			content: content
+		};
+		
+		this.messages.push(chatObject);
+		
+		return chatObject;
+	}
+}
